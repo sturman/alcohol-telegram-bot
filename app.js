7c828612de666df9cc4da30f6efdf0d756a47ec4
Roman Isko <iskoroman@gmail.com>
2016-07-04 15:35:41 +0300

    implemented send image for each product

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 var TelegramBot = require('node-telegram-bot-api');
 var request     = require('request-promise');
+var fs          = require('fs');
 
 var token    = '';
 var apiToken = '';
 // Setup polling way
 var bot      = new TelegramBot(token, {polling: true});
 
+var products;
+
 bot.onText(/\/find (.+)/, function (msg, match) {
     var fromId = msg.from.id;
     var query  = match[1].replace(' ', '+');
-    getProduct(query).then(function (result) {
-        var but      = {text: '1'};
-        var keyboard = {
-            keyboard         : [[but, but],
-                                [but]],
-            one_time_keyboard: true
-        };
-        bot.sendMessage(fromId, result, {reply_markup: JSON.stringify(keyboard)});
+    getProduct(query).then(function (data) {
+        products   = data;
+        var result = '';
+        for (var i = 0; i < data.length; i++) {
+            result += '/' + (i + 1) + '. ' + data[i].name + ' - ' + parseInt(data[i].alcohol_content) / 100 + '%\n';
+        }
+        bot.sendMessage(fromId, result);
     });
 });
 
-bot.onText(/^\d+$/, function (msg, match) {
+bot.onText(/\/\d+$/, function (msg, match) {
+    var id     = match[0].replace('/', '');
     var fromId = msg.from.id;
-    bot.sendMessage(fromId, msg.text, {reply_markup: JSON.stringify({hide_keyboard: true})});
+    getProductDescription(products[id - 1].id).then(function (data) {
+
+        var download = function (uri, filename, callback) {
+            request.head(uri, function (err, res, body) {
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+            });
+        };
+
+        var imageUrl = data.image_url;
+        if (imageUrl != null) {
+            download(imageUrl, './file.png', function () {
+                console.log('done');
+                bot.sendPhoto(fromId, './file.png');
+            });
+        } else {
+            bot.sendMessage(fromId, "Unfortunately, we do not have an image for " + data.name);
+        }
+    });
 });
 
 function getProduct(query) {
     var options = {
         uri    : 'http://lcboapi.com/products',
         qs     : {
-            per_page: 100,
+            per_page: 10,
             q       : query
         },
         headers: {
@@ -39,14 +59,27 @@ function getProduct(query) {
         json   : true
     };
 
-    var result = '';
+    return request(options)
+        .then(function (body) {
+            return body.data
+        });
+}
+
+function getProductDescription(id) {
+    var options = {
+        uri    : 'http://lcboapi.com/products/' + id,
+        qs     : {
+            per_page: 10
+        },
+        headers: {
+            'authorization': 'Token token="' + apiToken + '"',
+            'accept'       : 'application/vnd.api+json'
+        },
+        json   : true
+    };
 
     return request(options)
         .then(function (body) {
-            var data = body.data;
-            for (var i = 0; i < data.length; i++) {
-                result += i + 1 + '. ' + data[i].name + ' - ' + parseInt(data[i].alcohol_content) / 100 + '%\n';
-            }
-            return result
+            return body.data
         });
 }
