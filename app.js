5f340f67a2f6d52fbca1dd4a0a1cfc15a0c35500
Roman Isko <iskoroman@gmail.com>
2016-08-09 18:53:52 +0300

    fix variable declaration

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var bot      = new TelegramBot(token, {polling: true});
 var products;
 
 bot.onText(/\/start/, function (msg, match) {
-    var fromId    = msg.chat.id;
-    var firstName = msg.user.first_name;
-    welcomeMsg    = "Hello " + firstName + "!\n" +
-                    "I can try to find a short description and picture of any alcohol product\n" +
-                    "Just send me command /find <product_name>, e.g. /find heineken";
+    var fromId     = msg.chat.id;
+    var firstName  = msg.user.first_name;
+    var welcomeMsg = "Hello " + firstName + "!\n" +
+                     "I can try to find a short description and picture of any alcohol product\n" +
+                     "Just send me command /find <product_name>, e.g. /find heineken";
     bot.sendMessage(fromId, welcomeMsg);
 });
 
