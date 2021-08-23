8881357c7a7f5738c762d43f8f6ceb024b884fa6
Roman <4456572+sturman@users.noreply.github.com>
2021-08-23 17:16:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var TelegramBot = require('node-telegram-bot-api');
 var request     = require('request-promise');
-var fs          = require('fs');
+const fs          = require('fs');
 
 var token    = process.env.BOT_TOKEN;
 var apiToken = process.env.LSBO_TOKEN;
